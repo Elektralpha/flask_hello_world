73e6ca3da3a1894a4b0f7e1af364307b912b52bc
NicoISEP <82529463+Elektralpha@users.noreply.github.com>
2024-01-29 15:59:27 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template_string
+from flask import Flask
+from flask import render_template   
 import sqlite3
 
 app = Flask(__name__)                                                                                                                  
@@ -7,7 +8,49 @@ app = Flask(__name__)
 def hello_world():
     return render_template('hello.html')
 
+@app.route('/fr/')
+def hello_world_fr():
+    return "<h2>Bonjour tout le monde !</h2>"
 
+# Création d'une nouvelle route pour la lecture de la BDD
+@app.route('/lecture/')
+def ReadBDD():
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM livres;')
+    data = cursor.fetchall()
+    conn.close()
+
+   # Construisez dynamiquement une chaîne de modèle HTML
+    template_string = """
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Bibliotheque</title>
+    </head>
+    <body>
+        <h1>Contenu de la base de données</h1>
+        <table border="1">
+            <tr>
+                <th>ID</th>
+                <th>DATE</th>
+                <th>TITRE</th>
+                <th>AUTEUR</th>
+            </tr>
+            {% for row in data %}
+            <tr>
+                <td>{{ row[0] }}</td>
+                <td>{{ row[1] }}</td>
+                <td>{{ row[2] }}</td>
+                <td>{{ row[3] }}</td>
+            </tr>
+            {% endfor %}
+        </table>
+    </body>
+    </html>
+    """
+    return render_template_string(template_string, data=data)
                                                                                                                                        
 if __name__ == "__main__":
   app.run(debug=True)
